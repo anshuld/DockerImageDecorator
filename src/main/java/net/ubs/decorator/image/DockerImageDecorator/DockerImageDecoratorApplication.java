017530004a000148d6f44cc3292f293195f93ef0
Anshul Dutta <anshuld@gmail.com>
2024-09-08 09:52:58 +0100

    Intial Commit

---
+++ src/main/java/net/ubs/decorator/image/DockerImageDecorator/DockerImageDecoratorApplication.java
@@ -0,0 +1,13 @@
+package net.ubs.decorator.image.DockerImageDecorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImageDecoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageDecoratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ubs/decorator/image/DockerImageDecorator/DockerImageDecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ubs.decorator.image.DockerImageDecorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageDecoratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
